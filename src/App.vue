ea8affebdac2ac34d3f72a1f1680a5f146e7d4c3
gleyze <nataliegleyze@gmail.com>
2023-02-09 12:13:04 -0500

    pls work i cry

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,77 @@ import HelloWorld from './components/HelloWorld.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.btn{
+  padding: 5px;
+  margin: 5px; 
+  border: 5px solid pink;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-style: italic;
+  text-decoration: underline;
+}
+
+.btn:hover{
+  background-color: pink;
+}
+
+.btn:focus {
+  background-color: hotpink; 
+}
+
+.all{
+  border-style: dotted;
+  border-width: 10px;
+  border-color: pink;
+  width: 500px;
+  max-width: 600px;
+  margin: auto;
+  background-color: skyblue;
+}
+
+.title{
+  text-align: center;
+  font-size: 40px;
+}
+
+.header{
+  text-align: center;
+}
+.paragraph{
+   text-align: center;
+}
+img {
+  width: 400px;
+}
+
+p {
+  text-align: center; 
+  font-size: 20px;
+  padding: 15px;
+  color: red;
+}
+
+.detailButton {
+  padding: 5px;
+  margin: 5px; 
+  border: 5px solid pink;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-style: italic;
+  text-decoration: underline; 
+}
+
+  @media (min-width: 500px) and (max-width: 800px) {
+    
+    .detailButton {
+      display: none;
+    }
+
+@media (max-width: 500px) {
+  .all{
+    transform: scale(0.8);
+  }
+}
+
 </style>
